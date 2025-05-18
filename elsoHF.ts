8938f6bf334b764fd0761fe6cc2997213b9339f1
Hanna-ai591 <hanna.kariotis@gmail.com>
2025-05-18 19:19:42 +0200

    Add files via upload

---
+++ elsoHF.ts
@@ -0,0 +1,47 @@
+class Book{
+    constructor(
+        public id: string,
+        public title: string,
+        public author:string,
+        public price: number
+    ){}
+}
+
+interface ILibrary {
+    addBook(book: Book): void;
+    removeBook(id: string):void;
+    findBookById(id: string): Book  | undefined;
+    listAllBooks(): Book[];
+}
+class Library implements ILibrary{
+    books: Book[] = [];
+
+    addBook(book: Book):void{
+        this.books.push(book);
+    }
+    removeBook(id: string): void {
+        this.books = this.books.filter(book => book.id !== id);
+
+    }
+    findBookById(id: string):Book  | undefined{
+        return this.books.find(book => book.id == id);
+    }
+    listAllBooks(): Book[]{
+        return this.books;
+    }
+    
+}
+class User {
+    constructor(
+        public userId: string,
+        public name: string,
+        public email: string
+    ){}
+    borrowBook(library: Library, bookId: string): void {
+        const book = library.findBookById(bookId);
+        if (book) {
+          library.removeBook(bookId);
+        }
+    }
+
+}
